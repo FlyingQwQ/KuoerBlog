cbdec4688f4efb848543f18383e4de734102f989
FlyingQwQ <1275886165@qq.com>
2023-06-09 02:43:55 +0800

    little update.

--- Web/js/config.js
+++ Web/js/config.js
@@ -1,5 +1,7 @@
-var api = 'http://localhost:8081/';
-// var api = 'https://blogapi.kuoer.top:2333/';
+// var api = 'http://localhost:8081/';
+var api = 'https://blogapi.kuoer.top:2333/';
+var createTime = '2021 - 2023';
+var filingNumber = '粤ICP备2022013504号';
 
 setTimeout(function() {
     let loading = $('.loading');
@@ -10,6 +12,9 @@ setTimeout(function() {
 
 // 启用Bootstrap的tooltip功能
 $("[data-toggle='tooltip']").tooltip();
+
+$('#createTime').text(createTime);
+$('#filing').text(filingNumber);
 
 
 function addZero(num) {
